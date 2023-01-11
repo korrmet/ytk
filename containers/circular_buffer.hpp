9484e48b043a575e4ba174082daa56167dc067b1
Korrmet <cyka.blyat.ebat.moy.huy@gmail.com>
2023-01-11 13:41:03 +0300

    Add description to circular buffer

--- containers/circular_buffer.hpp
+++ containers/circular_buffer.hpp
@@ -24,6 +24,13 @@ class circular_buffer
 
     virtual ~circular_buffer() {}
 
+    /** \brief push referenced value to head of the buffer 
+     *
+     *  \param val reference to value that need to add
+     *
+     *  \return adding result
+     *  \retval true  value was added
+     *  \retval false value was not added */
     bool push_head(TYPE& val)
     { if (fullness >= VOLUME) { return false; }
 
@@ -38,6 +45,13 @@ class circular_buffer
 
       return true; }
 
+    /** \brief push value by pointer to head of the buffer
+     *  
+     *  \param val pointer to value that need to add
+     *
+     *  \return adding result
+     *  \retval true  value was added
+     *  \retval false value was not added */
     bool push_head(TYPE* val)
     { if (fullness >= VOLUME) { return false; }
 
@@ -52,10 +66,20 @@ class circular_buffer
 
       return true; }
 
+    /** \brief returns value on the top of the buffer
+     *
+     *  \return pointer to the value
+     *  \retval !nullptr valid pointer to the value
+     *  \retval nullptr  no value */
     TYPE* fetch_head(void)
     { if (!fullness) { return nullptr; }
       return &memory[head]; }
 
+    /** \brief deletes one value from the top of the buffer
+     *
+     *  \return result of deletion
+     *  \retval true  value was deleted
+     *  \retval false value was not deleted (nothing to delete) */
     bool pop_head(void)
     { if (!fullness) { return false; }
 
@@ -69,6 +93,13 @@ class circular_buffer
 
       return true; }
 
+    /** \brief add referenced value to the tail of the buffer
+     *  
+     *  \param val reference to value that need to add
+     *
+     *  \return result of addition
+     *  \retval true  value is added
+     *  \retval false value is not added (no place to add) */
     bool push_tail(TYPE& val)
     { if (fullness >= VOLUME) { return false; }
 
@@ -83,6 +114,13 @@ class circular_buffer
 
       return true; }
 
+    /** \brief add value by pointer to the tail of the buffer
+     *
+     *  \param val reference to value that need to add
+     *  
+     *  \return result of addition
+     *  \retval true  value is added
+     *  \retval false value is not added */
     bool push_tail(TYPE* val)
     { if (fullness >= VOLUME) { return false; }
 
@@ -97,10 +135,20 @@ class circular_buffer
 
       return true; }
 
+    /** \brief returns value on the bottom of buffer
+     * 
+     *  \return pointer to value
+     *  \retval !nullptr valid pointer to the value
+     *  \retval nullptr  no value */
     TYPE* fetch_tail(void)
     { if (!fullness) { return nullptr; }
       return &memory[tail]; }
 
+    /** \brief deletes value on the tail of the buffer
+     *
+     *  \return result of deletion
+     *  \retval truea value is deleted
+     *  \retval false value isn't deleted (nothing to delete) */
     bool pop_tail(void)
     { if (!fullness) { return false; }
 
@@ -114,14 +162,27 @@ class circular_buffer
 
       return true; }
 
+    /** \brief returns memory volume that is already used
+     *
+     *  \return number of elements */
     uint32_t memory_used() { return fullness; }
 
+    /** \brief returns free memory volume
+     *  
+     *  \return number of elements */
     uint32_t memory_available() { return VOLUME - fullness; }
 
   private:
+    /** \brief pointer to the memory that this buffer serves */
     TYPE* memory;
+    
+    /** \brief index of head element */
     uint32_t head;
+
+    /** \brief idex of tail memory */
     uint32_t tail;
+
+    /** \brief number of used elements */
     uint32_t fullness;
 };
 
@@ -139,6 +200,7 @@ class circular_buffer_static : public circular_buffer<TYPE, VOLUME>
   virtual ~circular_buffer_static() {}
 
   private:
+    /** \brief internal memory of the buffer */
     TYPE memory[VOLUME];
 };
 
